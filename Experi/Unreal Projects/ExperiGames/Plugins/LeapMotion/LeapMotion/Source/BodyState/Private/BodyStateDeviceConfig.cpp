0269b2680bc00ad20f5ec5cfd161bf10d7b6232f
Alex <71699507+StaplesAlex@users.noreply.github.com>
2023-02-01 13:16:35 +0000

    fix

--- Experi/Unreal Projects/ExperiGames/Plugins/LeapMotion/LeapMotion/Source/BodyState/Private/BodyStateDeviceConfig.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2020 Epic Games, Inc. All Rights Reserved.
-
-#include "BodyStateDeviceConfig.h"
-
-FBodyStateDeviceConfig::FBodyStateDeviceConfig() :
-	InputType(INERTIAL_INPUT_TYPE)
-{
-	DeviceName = FString(TEXT("Unknown"));
-}
