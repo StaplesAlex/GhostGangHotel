2ea6151ea9e49a4f220fdc2d32b683becb9b0b16
Alex <71699507+StaplesAlex@users.noreply.github.com>
2023-05-04 19:39:47 +0100

    Final Update?

--- GhostGang/Intermediate/Build/Win64/GhostGang/Development/Core/SharedPCH.Core.h
+++ GhostGang/Intermediate/Build/Win64/GhostGang/Development/Core/SharedPCH.Core.h
@@ -1,4 +1,4 @@
-// PCH for F:\UE_4.27\Engine\Source\Runtime\Core\Public\CoreSharedPCH.h
+// PCH for D:\UE_4.27\Engine\Source\Runtime\Core\Public\CoreSharedPCH.h
 #define IS_PROGRAM 0
 #define UE_GAME 1
 #define ENABLE_PGO_PROFILE 0
@@ -29,14 +29,14 @@
 #define WITH_PUSH_MODEL 0
 #define WITH_CEF3 1
 #define WITH_LIVE_CODING 1
-#define UE_LIVE_CODING_ENGINE_DIR "F:\\UE_4.27\\Engine"
-#define UE_LIVE_CODING_PROJECT "F:\\github\\GhostGang\\GhostGang\\GhostGang.uproject"
+#define UE_LIVE_CODING_ENGINE_DIR "D:\\UE_4.27\\Engine"
+#define UE_LIVE_CODING_PROJECT "C:\\Users\\s4002675\\Documents\\GitHub\\GhostGangHotel\\GhostGang\\GhostGang.uproject"
 #define UBT_MODULE_MANIFEST "UE4.modules"
 #define UBT_MODULE_MANIFEST_DEBUGGAME "UE4-Win64-DebugGame.modules"
 #define UBT_COMPILED_PLATFORM Win64
 #define UBT_COMPILED_TARGET Game
 #define UE_APP_NAME "UE4"
-#define UE_ENGINE_DIRECTORY "../../../../../UE_4.27/Engine/"
+#define UE_ENGINE_DIRECTORY "D:/UE_4.27/Engine/"
 #define NDIS_MINIPORT_MAJOR_VERSION 0
 #define WIN32 1
 #define _WIN32_WINNT 0x0601
@@ -58,4 +58,4 @@
 #define WITH_MALLOC_STOMP 1
 #define CORE_API 
 #define TRACELOG_API 
-#include "F:/UE_4.27/Engine/Source/Runtime/Core/Public/CoreSharedPCH.h"
+#include "D:/UE_4.27/Engine/Source/Runtime/Core/Public/CoreSharedPCH.h"
